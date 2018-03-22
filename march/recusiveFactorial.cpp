ded038e538cad33e66b23b2e538dd13af36edb0d
sakshammidha <37666420+sakshammidha@users.noreply.github.com>
2018-03-22 21:42:23 +0530

    first commit

---
+++ march/recusiveFactorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+long factorial(int n)
+{
+    if(n == 1)
+        return 1;
+
+    return n * factorial(n - 1);
+
+}
+
+int main()
+{
+    cout << factorial(5) << endl;
+    return 0;
+}
