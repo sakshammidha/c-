62c47c1b6b3a41e9559ac74272f72869feb1416a
sakshammidha <37666420+sakshammidha@users.noreply.github.com>
2018-06-18 20:43:05 +0530

    new

---
+++ june/x.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+//class definition
+class Complex
+{
+  //private members
+private:
+  int real;
+  int imaginary;
+
+  //public:
+public:
+  //constructors
+  //default constructor
+  Complex()
+  {
+    real = 10;
+    imaginary = 40;
+  }
+
+  //parameterised constructor
+  Complex(int r, int i)
+  {
+    real = r;
+    imaginary = i;
+  }
+
+  //copy constructor
+  Complex(Complex &s)
+  {
+    real = s.imaginary;
+    imaginary = s.real;
+  }
+
+  Complex(Complex &s, Complex &k)
+  {
+    real = s.real + k.real;
+    imaginary = s.imaginary + k.imaginary;
+  }
+
+  //destructor
+  ~Complex()
+  {
+    cout << "---------------------------------" << endl;
+    cout << real << " i" << imaginary << endl;
+    cout << "destructor called saksham" << endl;
+    cout << "---------------------------------" << endl;
+  }
+  void set(int r, int i)
+  {
+    real = r;
+    imaginary = i;
+  }
+
+  void get()
+  {
+    cout << real << " + i(" << imaginary << ")"<<endl;
+  }
+};
+
+int main()
+{
+  Complex c, d;
+  Complex e, f(45, 100);
+  Complex g(f);
+  Complex h = c;
+  Complex k(f, g);
+  c.set(3, 5);
+  d.set(5, -56);
+
+  c.get();
+  cout << endl;
+  d.get();
+  cout << endl;
+  e.get();
+  cout << endl;
+  f.get();
+  cout << endl;
+  g.get();
+  cout << endl;
+  h.get();
+  cout << endl;
+  k.get();
+  cout << endl;
+  return 0;
+}
